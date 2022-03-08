30e1e6bd6442b8bd187ff90af0a3029a91ef7095
Attachment Studios <72270541+Attachment-Studios@users.noreply.github.com>
2022-03-08 19:09:53 +0530

    PSEUDO 0.0.1 [DEV]

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 // Main
 
+function redirect(page) {
+	window.location.href = `/${page}`;
+}
+
+function download(version) {
+	fetch(`https://api.synt.ml/download?version=${version}`).then(request => request.text()).then(link => {
+		if (link !== 'ERROR') {
+			window.location.open(link);
+		} else {
+			alert('ERROR!');
+		}
+	}).catch(error => {
+		console.log("FAILED TO CONNECT: API.SYNT.ML (CONNECT SITE)");
+		fetch(`https://synt-api.attaditya.repl.co/download?version=${version}`).then(request => request.text()).then(link => {
+			if (link !== 'ERROR') {
+				window.location.open(link);
+			} else {
+				alert('ERROR!');
+			}
+		}).catch(error => {
+			console.log("FAILED TO CONNECT: API.SYNT.ML (HOST SITE)");
+			window.open(`https://github.com/SyntLang/Versions`);
+		});
+	});
+}
+
